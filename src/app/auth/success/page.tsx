d1304ad475661713c888e342a588a30d80888b62
NlengDominique <luqnleng5@gmail.com>
2025-11-24 16:33:42 +0100

    handle oauth2 token

--- src/app/auth/success/page.tsx
+++ src/app/auth/success/page.tsx
@@ -1,18 +1,43 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function SuccessPage() {
-  const router = useRouter();
+    const router = useRouter();
+    const params = useSearchParams();
+    const code = params.get("code");
 
-  useEffect(() => {
-    router.replace("/dashboard");
-  }, [router]);
+    const [error, setError] = useState<string | null>(null);
+    useEffect(() => {
+        if (!code) {
+            setError("Code manquant");
+            return;
+        }
+        async function exchangeCode() {
+            try {
+                const res = await fetch(
+                    `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/otp/oauth2/exchange?email=&code=${code}`,
+                    {
+                        method: "POST",
+                        credentials: "include",
+                    }
+                );
 
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p className="text-gray-600">Connexion en cours…</p>
-    </div>
-  );
+                if (!res.ok) {
+                    const data = await res.json();
+                    throw new Error(data.message || "Erreur lors de l'échange du code");
+                }
+                router.replace("/dashboard");
+            } catch (err: any) {
+                console.error(err);
+                setError(err.message);
+            }
+        }
+        exchangeCode();
+
+    }, [code, router]);
+
+    if (error) return <p className="text-red-500">{error}</p>;
+    return <p>Connexion en cours…</p>;
 }
